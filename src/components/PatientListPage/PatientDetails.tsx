6577ed23665d1e0de45be6cd9b72d3a937609b01
keyxcode <keyxcode@gmail.com>
2023-05-29 17:47:16 +0700

    Solution 9.23: list entry's date, description and diagnoseCodes

--- src/components/PatientListPage/PatientDetails.tsx
+++ src/components/PatientListPage/PatientDetails.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import patientService from '../../services/patients'
-import { Patient } from '../../types'
-import { Container, Typography } from '@mui/material'
+import { Patient, Entry } from '../../types'
+import { Container, Typography, Box } from '@mui/material'
 import { Female, Male } from '@mui/icons-material';
 
 const PatientDetaills = () => {
@@ -34,6 +34,17 @@ const PatientDetaills = () => {
             <Typography variant="body1">
                 occupation: {patient?.occupation}
             </Typography>
+            <Typography component="h2" variant="h5" style={{ marginTop: 16 }}>entries</Typography>
+            {patient?.entries.map(entry => (
+                <Container style={{ padding: 0 }}>
+                    <Typography display="inline" variant="body1">{entry.date}</Typography>
+                    <Typography display="inline" fontStyle="italic"> {entry.description}</Typography>
+                    <ul>
+                        {entry.diagnosisCodes?.map(code => (<li>{code}</li>))}
+                    </ul>
+                </Container>
+            ))}
+
         </Container>)
 }
 
