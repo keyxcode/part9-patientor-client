5618f1b62e2810ef0f012c8ee3493c17c4926b1e
keyxcode <keyxcode@gmail.com>
2023-05-31 18:30:21 +0700

    Support all entry types

--- src/components/AddEntryModal/AddEntryForm.tsx
+++ src/components/AddEntryModal/AddEntryForm.tsx
@@ -1,6 +1,6 @@
 import { useState, SyntheticEvent } from "react";
-import { TextField, InputLabel, MenuItem, Select, Grid, Button, FormControl, SelectChangeEvent } from '@mui/material';
-import { NewHealthEntry, Gender, HospitalEntry, HealthCheckEntry } from "../../types";
+import { TextField, MenuItem, Select, Grid, Button, FormControl, Box, Typography } from '@mui/material';
+import { NewHealthEntry, HealthCheckEntry } from "../../types";
 
 interface Props {
     onCancel: () => void;
@@ -14,12 +14,31 @@ const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
         specialist: "",
         type: "HealthCheck",
         diagnosisCodes: [],
-        healthCheckRating: 0
+        healthCheckRating: 0,
+        dischargeDate: "",
+        dischargeCriteria: "",
+        employerName: "",
+        startDate: "",
+        endDate: "",
     })
 
     const addPatient = (event: SyntheticEvent) => {
         event.preventDefault();
-        onSubmit(formData as Omit<HealthCheckEntry, "id">);
+
+        let dataToSubmit = {}
+
+        if (formData.type === "HealthCheck") {
+            const { dischargeDate, dischargeCriteria, employerName, startDate, endDate, ...rest } = formData
+            dataToSubmit = { ...rest }
+        } else if (formData.type === "Hospital") {
+            const { healthCheckRating, employerName, startDate, endDate, dischargeDate, dischargeCriteria, ...rest } = formData
+            dataToSubmit = { ...rest, discharge: { date: dischargeDate, criteria: dischargeCriteria } }
+        } else if (formData.type === "OccupationalHealthcare") {
+            const { healthCheckRating, dischargeDate, dischargeCriteria, startDate, endDate, ...rest } = formData
+            dataToSubmit = { ...rest, sickLeave: { startDate, endDate } }
+        }
+
+        onSubmit(dataToSubmit as NewHealthEntry);
     };
 
     const handleChange = (event: { target: { name: any; value: any; }; }) => {
@@ -74,8 +93,49 @@ const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
                         value={formData.healthCheckRating}
                         onChange={handleChange}
                     />}
-                {formData.type === "Hospital" && "Hospital"}
-                {formData.type === "OccupationalHealthcare" && "OccupationalHealthcare"}
+                {formData.type === "Hospital" &&
+                    <Box>
+                        <Typography>discharge</Typography>
+                        <TextField
+                            label="discharge date"
+                            name="dischargeDate"
+                            fullWidth
+                            value={formData.dischargeDate}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            label="discharge criteria"
+                            name="dischargeCriteria"
+                            fullWidth
+                            value={formData.dischargeCriteria}
+                            onChange={handleChange}
+                        />
+                    </Box>}
+                {formData.type === "OccupationalHealthcare" &&
+                    <Box>
+                        <Typography>sick leave</Typography>
+                        <TextField
+                            label="employer name"
+                            name="employerName"
+                            fullWidth
+                            value={formData.employerName}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            label="start date"
+                            name="startDate"
+                            fullWidth
+                            value={formData.startDate}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            label="end date"
+                            name="endDate"
+                            fullWidth
+                            value={formData.endDate}
+                            onChange={handleChange}
+                        />
+                    </Box>}
                 <Grid>
                     <Grid item>
                         <Button
